647b5541e59907d9bd3c4fd5e972351a196993de
Jeremy <ing.jeremy.quiel@gmail.com>
2026-08-29 18:18:08 -0600

    feat: add environments variables

---
+++ aegis-admin-frontend/src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://localhost:8080',
+  keycloakUrl: 'http://localhost:8081',
+  keycloakRealm: 'aegis',
+  keycloakClientId: 'aegis-dev-cli'
+};
+
